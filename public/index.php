6d6a9553942caeb416f4ba57b7589a499ff013ed
Syafiq Naim <syafiqnaim@Syafiqs-MBP.realtek>
2022-03-17 17:38:23 +0800

    test 1 nginx

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 */
 
 require __DIR__.'/../vendor/autoload.php';
-//test 2 nginx
+//test 1 nginx
 /*
 |--------------------------------------------------------------------------
 | Run The Application
