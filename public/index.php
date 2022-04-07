81b1f9629d23e99166e89f6ab8bb6952e4d84924
Syafiq Naim <syafiqnaim@Syafiqs-MacBook-Pro.local>
2022-04-07 11:11:20 +0800

    07042022

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 */
 
 require __DIR__.'/../vendor/autoload.php';
-//test 05042022-3
+//test 07042022
 /*
 |--------------------------------------------------------------------------
 | Run The Application
