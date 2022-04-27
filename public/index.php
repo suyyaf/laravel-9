21177cdf80924bbadec66c3db320a21a59fade86
Syafiq Naim <syafiqnaim@Syafiqs-MBP.realtek>
2022-04-27 10:00:11 +0800

    27042022-4

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 */
 
 require __DIR__.'/../vendor/autoload.php';
-//test 27042022-1
+//test 27042022-2
 /*
 |--------------------------------------------------------------------------
 | Run The Application
