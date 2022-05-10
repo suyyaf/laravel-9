c65f2125427a1abbc75cafa821826c5fed5581bd
Syafiq Naim <syafiqnaim@Syafiqs-MBP.realtek>
2022-05-10 14:34:49 +0800

    push

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-echo "uno2";
+echo "uno3";
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
